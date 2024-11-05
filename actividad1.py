e7a1ca6589db184a6558534adf7afbbda44a5a97
HuGod-codes <hitorricos@miuandes.cl>
2024-11-05 18:23:14 -0300

    Update act1

--- actividad1.py
+++ actividad1.py
@@ -1,7 +1,6 @@
 import pandas as pd
-
-
-# ------ Lectura y analisis exploratorio de datos -------
+import random
+import sklearn
 
 def thingToNum(value):
     num = 0
@@ -10,46 +9,78 @@ def thingToNum(value):
         if i == '.':
             return num
         elif i in digitos:
-            num = num * 10 + int(i) 
+            num = num * 10 + int(i)
     return num
 
-df = pd.read_csv("datos/ejemplo_data.csv")
-print(df.dtypes)
+# ------ Lectura y analisis exploratorio de datos -------
 
-df = df.astype({'ID':'int', 'Activo':'bool'})
-print(df.dtypes)
+# df = pd.read_csv("datos/ejemplo_data.csv")
+# print(df.dtypes)
 
-print(df)
+# df = df.astype({'ID':'int', 'Activo':'bool'})
+# print(df.dtypes)
 
-df['Unidades'] = df['Unidades'].apply(thingToNum)
-df['2016'] = df['2016'].apply(thingToNum)
-df = df.astype({'Unidades':'int', '2016':'float'})
+# print(df)
 
-print(df)
-print(df.dtypes)
+# df['Unidades'] = df['Unidades'].apply(thingToNum)
+# df['2016'] = df['2016'].apply(thingToNum)
+# df = df.astype({'Unidades':'int', '2016':'float'})
+
+# print(df)
+# print(df.dtypes)
 
 # ---- Lectura y analisis exploratorio de datos 2  -----
 
-df2 = pd.read_csv("datos/ecommerce_data.csv", parse_dates = [4], date_format = "%d/%m/%y %H:%M", encoding = 'unicode_escape')
-print(df2.dtypes)
+# df2 = pd.read_csv("datos/ecommerce_data.csv", parse_dates = [4], date_format = "%d/%m/%y %H:%M", encoding = 'unicode_escape')
+# print(df2.dtypes)
 
-print(df2[:10])
+# print(df2[:10])
 
-df2['InvoiceNo'] = df2['InvoiceNo'].apply(thingToNum)
-df2 = df2.astype({'InvoiceNo':'int', 'Description':'str'})
-print(df2.dtypes)
+# df2['InvoiceNo'] = df2['InvoiceNo'].apply(thingToNum)
+# df2 = df2.astype({'InvoiceNo':'int', 'Description':'str'})
+# print(df2.dtypes)
 
-df2 = df2.astype({'Quantity':'int', 'UnitPrice':'float'})
+# df2 = df2.astype({'Quantity':'int', 'UnitPrice':'float'})
 
-df2[['Date', 'Time']] = df2['InvoiceDate'].str.split(' ', expand=True)
-del df2['InvoiceDate']
+# df2[['Date', 'Time']] = df2['InvoiceDate'].str.split(' ', expand=True)
+# del df2['InvoiceDate']
 
-df2['Price'] = df2['Quantity'] * df2['UnitPrice']
+# df2['Price'] = df2['Quantity'] * df2['UnitPrice']
+# print(df2[140:155])
 
-print(df2[140:155])
+# df2.to_csv('new_ecommerce_data.csv', index=False)
 
-df2.to_csv('new_ecommerce_data.csv', index=False)
+# - Exploracion de funciones de pandas -
 
 # ---------  Estadisticas Descriptivas  ----------
 
+# people = {'ID': [], 'Age': [], 'Height': [], 'Grade': []}
 
+# for i in range(50):
+
+#     n = [random.randint(65,90) for _ in range(4)]
+
+#     id = chr(n[0])
+#     for l in random.choices(range(10), k = 6):
+#         id += str(l)
+
+#     people['ID'].append(id)
+#     people['Age'].append(n[1]-47)
+#     people['Height'].append(1+n[2]/100)
+#     people['Grade'].append(99*(n[3]-65)/25+1)
+
+# people['Approved'] = ['YES' if grade >= 51 else 'NO' for grade in people['Grade']]
+
+# dfPeople = pd.DataFrame(people)
+
+# print(dfPeople.describe())
+# print(dfPeople.var(numeric_only=True))
+# print(dfPeople.cov(numeric_only=True))
+
+# ------  Transformacion e Imputacion de datos  -------
+
+dfR = pd.read_csv("datos/ratings_data.csv")
+dfB = pd.read_csv("datos/books_data.csv", sep=';', encoding = 'unicode_escape')
+
+dfB.info()
+print(dfB.isna().sum())
